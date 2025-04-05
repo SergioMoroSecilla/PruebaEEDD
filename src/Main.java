95ec1e8e9ae7f9690bc3db54e3a165e3491b954b
Sergio Moro <sergiomoro7@icloud.com>
2025-04-05 19:26:38 +0200

    s

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("Suma: " + calc.sumar(5, 10));
         System.out.println("División: " + calc.dividir(10, 0)); // Error de división por 0
 
-        System.out.prinln("otro mensajito prueba para comits GIT")
+        System.out.prinln("otro mensajito prueba comits GIT")
     }
 }
